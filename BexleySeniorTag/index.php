488aaaab83f4e7dd451959d312c082a1a241cc46
Alec Robins <alecrobins@gmail.com>
2014-12-22 00:12:43 -0500

    Update index.php

--- BexleySeniorTag/index.php
+++ BexleySeniorTag/index.php
@@ -6,19 +6,6 @@
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
 <title>BST - Bexely Senior Tag 2012</title>
 
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-31220396-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
 </head>
 
 <body>
